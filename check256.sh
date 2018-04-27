b4e2d294fc624eb400cb95fe7c7c2688ac910fe1
Cj-bc <cj-bc@y-modify.org>
2018-04-27 11:24:44 +0900

    Add: check256 for git based program

---
+++ check256.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 2 ]
+then
+  wget -q "https://github.com/${1}/archve/${2}.tar.gz"
+  echo `openssl dgst -sha256 ${2}.tar.gz`
+  rm ${2}.tar.gz
+else
+    echo "usage: check256 <user/repo> <version>"
+    echo "        if you use tag like 'v0.1.0', give all of it"
+fi 
